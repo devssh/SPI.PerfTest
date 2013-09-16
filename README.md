707cad54e62fc0a3b704e1835d8400307c1d0195
Balaji G <gbalaji@thoughtworks.com>
2013-09-16 15:10:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Versions
+------------
+
+The current version of the project is based on the Gatling version : **2.0.0-M3a**.
+
+Usage
+-----
+
+Checkout the project and run the simulations with the following command :
+
+```
+mvn gatling:execute
+```
+
+To run a specific simulation you have to use this command :
+
+```
+mvn gatling:execute -Dgatling.simulation=basic.BasicExampleSimulation
+```
---
+++ src/test/scala/spicinemas/NowShowingSimulation.scala
@@ -0,0 +1,35 @@
+package spicinemas
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import assertions._
+import bootstrap._
+
+class NowShowingSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://10.16.3.236")
+    .acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+    .disableFollowRedirect
+    .shareConnections
+
+  val scn = scenario("Get now showing")
+    .exec(
+    http("request_1")
+      .get("/chennai/now-showing")
+      .check(
+      css( """ul.movie__grid li.movie__grid__item:first-child a.filter-value""", "href").exists.saveAs("movie_url"),
+      status.is(200)))
+   .exec(
+    http("request_2")
+      .get("${movie_url}")
+      .check(
+      status.is(200),
+      css(""" div.movie__title """).exists
+    ))
+  setUp(scn.inject(ramp(100 users) over (10 seconds))).protocols(httpConf)
+}
