2085f147f1346dbd53dbb999ae392d78b3aa6513
govindr <govindr@INgovindr.local>
2014-05-27 15:12:37 +0530

    quick book, showtime with layout, bookedOrders simulations

---
+++ src/test/scala/spicinemas/simulations/BookedOrdersSimulation.scala
@@ -0,0 +1,123 @@
+package spicinemas.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import assertions._
+import bootstrap._
+import spicinemas.utils.Properties._
+
+class BookedOrdersSimulation extends Simulation {
+  val httpConf = http
+    .baseURL(baseUrl)
+    .acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+    .disableFollowRedirect
+    .shareConnections
+  
+  val currentDate = "19-05-2014";
+  val headerAjax = Map(
+        "X-Requested-With" -> """XMLHttpRequest""",
+        """Cookie""" -> """cityName=chennai"""
+    )
+
+  val headerAjaxForm = Map(
+      "Cache-Control" -> """no-cache""",
+      "Content-Type" -> """application/x-www-form-urlencoded; charset=UTF-8""",
+      "Pragma" -> """no-cache""",
+      "Cookie" -> "cityName=chennai",
+      "X-Requested-With" -> """XMLHttpRequest"""
+  )
+
+  val headerAjaxJson = Map(
+        "Accept" -> """application/json, text/javascript, */*; q=0.01""",
+        "Cache-Control" -> """no-cache""",
+        "Content-Type" -> """application/json; charset=UTF-8;""",
+            "Origin" -> s"""http://${domain}""",
+            "Referer" -> s"""http://${domain}/chennai""",
+            "Host" -> s"""${domain}""",
+            "Cookie" -> """cityName=chennai""",
+          "X-Requested-With" -> """XMLHttpRequest"""
+  )
+
+  val hearderForJsonGet = Map(
+        "Accept" -> """application/json, text/javascript, */*; q=0.01""",
+        "Content-Type" -> """application/json;""",
+        "X-Requested-With" -> """XMLHttpRequest"""
+  )
+
+    val bookedOrderFeeder = csv("orders_booked.csv").random    
+    val scn = scenario("quick book")        
+    .exec(http("account-logged")
+        .get("/account/logged")
+        .headers(headerAjax)
+        .check(status.is(401))
+    )
+    .pause(500 milliseconds)
+    .exec(session => {
+      import java.net.URI      
+      import io.gatling.http.cookie._;
+      import com.ning.http.client._;
+
+      val customCookie1 = new Cookie(domain, "cityName", "chennai", "/", 864000, false)
+      val customCookie2 = new Cookie(domain, "selectedCity", "chennai", "/", 864000, false)
+      val cookieStore = CookieJar(new URI(baseUrl), List(customCookie1, customCookie2))
+      session.set("gatling.http.cookies", cookieStore)
+    })
+    .feed(bookedOrderFeeder)
+    .exec(http("authenticate")
+        .post("/account/authenticate")
+        .headers(headerAjaxForm)
+        .param("user", """${username}""")
+        .param("password", """twpass""")
+    .check(status.is(200)))    
+    .exec(http("get sessions")
+      .get("/chennai/sessions")                    
+      .check(status.is(200))
+    )    
+    .exec(http("post sessions")
+      .post("/chennai/sessions")
+      .headers(headerAjaxJson)
+        .body(StringBody("""{"cinema":"${cinema_name}","date":"""" + s"${currentDate}" + """","movie":"${movie_name}"}""")).asJSON
+      .check(status.is(200))
+    )
+    .pause(500 milliseconds)        
+    .exec(http("order status")
+      .post("/order/status")
+      .headers(headerAjaxJson)
+      .body(StringBody("""{"sessionId":"${session_id}","quantity":"1","seatCategory":"ELITE"}""")).asJSON
+      .check(status.is(200))
+    )
+    .exec(http("orderDetail")
+      .get("/order/details/${orderid}")      
+      .headers(hearderForJsonGet)              
+      .queryParam("""sessionId""","""${session_id}""")
+      .queryParam("""quantity""","""1""")
+      .queryParam("""seatCategory""","""ELITE""")            
+      .check(status.is(200)))
+    .exec(http("get food")
+      .post("/food")      
+      .headers(headerAjaxJson)              
+      .body(StringBody("""$cinema_name""")).asJSON
+      .check(status.is(200)))        
+    // .exec(http("buy food")
+    //   .post("/food/buy")
+    //   .headers(headerAjaxJson)      
+    //   .body(StringBody("""{"orderId":"$orderId","foodWithQty":{"578":1,"623":1}}""")).asJSON
+    //   .check(status.is(200))
+    // )
+    .exec(http("get payment options")
+      .get("/payment/options")
+      .check(status.is(200))
+    )
+    // .pause(300 milliseconds)
+    // .exec(http("cancel ticket")
+    //   .post("/chennai/ticket/cancel")
+    //   .headers(headerAjaxJson)
+    //   .body(StringBody("""{"sessionId":"${session_id}","selectedCity":"chennai","orderId":${orderId},"seatCategory":"ELITE","quantity":"1"}""")).asJSON          
+    //   .check(status.is(200))
+    // )    
+  setUp(scn.inject(ramp(200 users) over (5 seconds))).protocols(httpConf)
+}
--- src/test/scala/spicinemas/simulations/ShowTimeSimulation.scala
+++ src/test/scala/spicinemas/simulations/ShowTimeSimulation.scala
@@ -16,8 +16,9 @@ class ShowTimeSimulation extends Simulation {
     .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
     .disableFollowRedirect
     .shareConnections
-
+  
   val domain = "devtest.spicinemas.in"
+  val currentDate = "20-05-2014";
   val headers_5 = Map(
   			"X-Requested-With" -> """XMLHttpRequest""",
         """Cookie""" -> """cityName=chennai"""
@@ -44,21 +45,21 @@ class ShowTimeSimulation extends Simulation {
 
   val hearderForJsonGet = Map(
     		"Accept" -> """application/json, text/javascript, */*; q=0.01""",
-    		"Cache-Control" -> """no-cache""",      
-        "Cookie" -> """cityName=chennai"""
+        "Content-Type" -> """application/json;""",
+        "X-Requested-With" -> """XMLHttpRequest"""
   )
 
     val movieFeeder = csv("movie_name.csv").random
     val userFeeder = csv("user_credentials.csv").random
 
     val scn = scenario("Show Times")    
-    .exec(http("show-times")
-			.get("/chennai/show-times/15-05-2014")
-        .queryParam("seats","2")
-			.check(status.is(200))
-	  )    
+  //   .exec(http("show-times")
+		// 	.get(s"/chennai/show-times/$currentDate")
+  //       .queryParam("seats","2")
+		// 	.check(status.is(200))
+	 //  )    
 
-		.pause(500 milliseconds)
+		// .pause(500 milliseconds)
 
     .exec(http("account-logged")
         .get("/account/logged")
@@ -82,39 +83,50 @@ class ShowTimeSimulation extends Simulation {
         .param("user", """${username}""")
         .param("password", """${password}""")
     .check(status.is(200)))
-
-    .pause(500 milliseconds)
-
+  //  .pause(500 milliseconds)
+    
     .feed(movieFeeder)
-    .exec(http("book")
-      .post("/chennai/ticket/${movie_name}/book")
-    	.param("""sessionId""", """${session_id}""")
-   		.param("""seatCategory""", """ELITE""")
-   		.param("""quantity""", """2""")
-      .param("""cityName""","""chennai""")
-      .check(status.is(200), css("""#orderId""","value").exists.saveAs("orderId"))
+    .exec(http("order status")
+      .post("/order/status")
+      .headers(headers_12)
+      .body(StringBody("""{"sessionId":"${session_id}","quantity":"2","seatCategory":"ELITE"}""")).asJSON
+      // .param("""sessionId""", """${session_id}""")
+      // .param("""seatCategory""", """ELITE""")
+      // .param("""quantity""", """2""")
+      .check(status.is(200))
     )
- 	  .pause(1000 milliseconds)
+    //.pause(300 milliseconds)
 
-    // .exec(http("orderDetail")
-    //   .get("/order/details")      
-    //   .headers(hearderForJsonGet)              
-    //   .queryParam("""sessionId""","""${session_id}""")
-    //   .queryParam("""quantity""","""2""")
-    //   .queryParam("""seatCategory""","""ELITE""")            
-    //   .check(status.is(200)))      
-    //   // .check(jsonPath("$.orderId").saveAs("orderId")))
+    .exec(http("book")
+          .post("/chennai/ticket/${movie_name}/book")
+        	.param("""sessionId""", """${session_id}""")
+       		.param("""seatCategory""", """ELITE""")
+       		.param("""quantity""", """2""")
+          .param("""cityName""","""chennai""")
+          .param("""selectedCity""","""chennai""")
+          .check(status.is(200), css("""#orderId""","value").exists.saveAs("orderId"))
+        )
+     	  .pause(1000 milliseconds)
 
-  // .exec(http("layout")
-  //    .post("/screen/layout")
-  //    .headers(headers_12)
-  //    .param("sessionId", """${session_id}""")
-  //    .param("quantity", """2""")
-  //    .param("seatCategory", """ELITE""")
-  //    .param("orderId", """${orderId}""")
-  //    .param("orderId", """false""")
-  //    // .body(StringBody("""{"sessionId":"${session_id}","quantity":"2","seatCategory":"ELITE","orderId":"${orderId}"}""")).asJSON
-  //    .check(status.is(200)))
+    .exec(http("orderDetail")
+      .get("/order/details")      
+      .headers(hearderForJsonGet)              
+      .queryParam("""sessionId""","""${session_id}""")
+      .queryParam("""quantity""","""2""")
+      .queryParam("""seatCategory""","""ELITE""")            
+      .check(status.is(200)))      
+      // .check(jsonPath("$.orderId").saveAs("orderId")))
 
-  setUp(scn.inject(ramp(2000 users) over (5 seconds))).protocols(httpConf)
+  .exec(http("layout")
+     .post("/screen/layout")
+     .headers(headers_12)
+     // .param("sessionId", """${session_id}""")
+     // .param("quantity", """2""")
+     // .param("seatCategory", """ELITE""")
+     // .param("orderId", """${orderId}""")
+     // .param("isAutoSelected", """false""")
+     .body(StringBody("""{"sessionId":"${session_id}","quantity":"2","seatCategory":"ELITE","orderId":"${orderId}","isAutoSelected":false}""")).asJSON
+     .check(status.is(200)))
+
+  setUp(scn.inject(ramp(2000 users) over (100 seconds))).protocols(httpConf)
 }
