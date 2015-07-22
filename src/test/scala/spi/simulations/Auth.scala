adedbdc663607a813b08a0fce76857a3af787bb5
deepak <anandhadeepak@gmail.com>
2015-07-22 22:48:43 +0530

    deepak - auth test, login eventually and get user in burst mode

--- src/test/scala/spi/simulations/Auth.scala
+++ src/test/scala/spi/simulations/Auth.scala
@@ -29,16 +29,19 @@ class Auth extends Simulation {
     .exec(loginPage)
     .exec(userAuthentication)
     .exec(getAuthorizationToken)
+    .exitHereIfFailed
     .exec(storeAuthTokenToRedis)
 
 
-  val getUserInfo = scenario("UserInfo").feed(authTokenFeeder).exec(setAuthCookie)
+  val getUserInfo = scenario("UserInfo").feed(authTokenFeeder).
+    exitHereIfFailed.
+    exec(setAuthCookie)
     .repeat(5){exec(userInfo)}
 
 
   setUp(
     userLogin.inject(rampUsers(2200) over (100 seconds)),
-    getUserInfo.inject(nothingFor(120),atOnceUsers(2000))
+    getUserInfo.inject(nothingFor(200),atOnceUsers(2000))
 //    userLogin.inject(rampUsers(20) over (10 seconds)),
 //        getUserInfo.inject(nothingFor(20 seconds),atOnceUsers(15))
   ).protocols(httpConf)
